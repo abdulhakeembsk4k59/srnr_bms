512cb3c2df932df72be2cfe8d056f1be34a232b5
abdulhakeembsk4k59 <sk.a.hakeem123@gmail.com>
2025-03-05 10:09:22 +0530

    inital commit

---
+++ src/com/srnr/bms/main/TestMain.java
@@ -0,0 +1,12 @@
+package com.srnr.bms.main;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Yo there!");
+
+	}
+
+}
