572a3a987847901a9542fcacce26906254c079e2
venu kumar <k.venukumar6543@gmail.com>
2025-03-05 12:02:47 +0530

    third commit adding one method in service layer

--- src/com/srnr/bms/service/ServiceLayer.java
+++ src/com/srnr/bms/service/ServiceLayer.java
@@ -1,6 +1,15 @@
 package com.srnr.bms.service;
 
 public class ServiceLayer {
+	
+	
+	public void serviceLayerMethod()
+	{
+		System.out.println("ServiceLayer.serviceLayerMethod()");
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		System.out.println("*************************");
 		System.out.println("Service Layer---- Venu");
@@ -8,3 +17,4 @@ public class ServiceLayer {
 	}
 
 }
+
