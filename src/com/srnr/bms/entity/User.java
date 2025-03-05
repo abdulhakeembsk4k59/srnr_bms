10550ae2c49dbfcc0a19db2b8232d4ded47287a7
Sujit Maharana <sujitmaharana1111@gmail.com>
2025-03-05 11:48:36 +0530

    added some entity properties

--- src/com/srnr/bms/entity/User.java
+++ src/com/srnr/bms/entity/User.java
@@ -1,18 +1,23 @@
 package com.srnr.bms.entity;
 
+import java.io.Serializable;
 
-public class User {
+@SuppressWarnings("serial")
+public class User implements Serializable{
 	
 	private Integer id;
+	private String userName;
+	
 
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public User(Integer id) {
+	public User(Integer id,String userName) {
 		super();
 		this.id = id;
+		this.userName=userName;
 	}
 
 	public Integer getId() {
@@ -23,10 +28,21 @@ public class User {
 		this.id = id;
 	}
 
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + "]";
+		return "User [id=" + id + ", userName=" + userName + "]";
 	}
+
+	
 	
 
 }
